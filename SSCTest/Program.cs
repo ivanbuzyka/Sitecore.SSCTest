5d860adb37b943759d3f88962bec610a9915d54b
ivanbuzyka <ivanbuzyka@gmail.com>
2022-08-18 13:19:27 +0200

    Add project files.

---
+++ SSCTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace SSCTest
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // this tools calls Sitecore Services Client:
+      // first request: POST to the login endpoint /sitecore/api/ssc/auth/login
+      // second request the one you can pass through parameters. Note - that should be an URL for GET request only
+      if (args.Length == 0)
+      {
+        Console.WriteLine("No parameters passed.");
+        Console.WriteLine("Pass arguments in following order: https://cmurl domain name username password urlToGETSubsequentlyAfterLogin");
+        return;
+      }
+
+      var loginUrl = $"{args[0]}/sitecore/api/ssc/auth/login";
+      var domain = args[1];
+      var userName = args[2];
+      var password = args[3];
+      var secondRequestUrl = args[4];
+
+      using (HttpClient client = new HttpClient())
+      {
+        StringContent authenticationjson = new StringContent("{ \"domain\": \"" + domain + "\", \"username\": \"" + userName + "\", \"password\": \"" + password + "\" }", Encoding.UTF8, "application/json");
+        HttpResponseMessage authResult = client.PostAsync(loginUrl, authenticationjson).Result;
+
+        if (authResult.StatusCode != HttpStatusCode.OK)
+        {
+          Console.WriteLine("Login NOt successfull");
+          Console.WriteLine($"Login response status: {authResult.StatusCode}");          
+        }
+
+        HttpResponseMessage getItemResult = client.GetAsync(secondRequestUrl).Result;
+
+        if (authResult.StatusCode != HttpStatusCode.OK)
+        {
+          Console.WriteLine($"Error when fetching the URL. Status: {authResult.StatusCode}");
+        }
+
+        var result = getItemResult.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine($"Response content: {result}");
+      }
+    }
+  }
+}
---
+++ SSCTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SSCTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sitecore Corp")]
+[assembly: AssemblyProduct("SSCTest")]
+[assembly: AssemblyCopyright("Copyright © Sitecore Corp 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e42f3222-ce68-4374-bfb9-637ee04beb89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
